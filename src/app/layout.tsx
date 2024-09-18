5d1ffc58aaf8e6f82792be0fa408758a5a1c9d4c
Shubham-Tiwari-Anywhere <shubham.prem@anywhere.co>
2024-09-18 14:15:44 +0530

    added pagespeed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import NavbarComponent from "@/components/Navbar";
 import { NextUIProvider } from "@nextui-org/react";
 import QueryClientWrapper from "@/components/Blogs/QueryClientWrapper";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({
             </main>
           </NextUIProvider>
         </QueryClientWrapper>
+        <SpeedInsights />
       </body>
     </html>
   );
