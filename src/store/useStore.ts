228c6d78c82869bdda5e873731b3a0dd9c1aa121
Shubham-Tiwari-Anywhere <shubham.prem@anywhere.co>
2024-09-24 14:14:47 +0530

    Minor component updates

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { devtools, persist } from "zustand/middleware";
 
-type User = {
+export type User = {
   uid: string | null;
   displayName: string | null;
   email: string | null;
